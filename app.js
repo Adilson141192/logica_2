517e45ca72a1d994ce1c98c0333292415a42c31f
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-08-07 14:56:20 -0300

    080724 aula0403

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let listaNumerosSorteados = [];
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -34,7 +35,14 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * 4 + 1);
+    if (listaNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaNumerosSorteados.push(numeroEscolhido);
+        console.log(listaNumerosSorteados);
+        return numeroEscolhido;
+    }
 }
 
 function limparCampo() {
